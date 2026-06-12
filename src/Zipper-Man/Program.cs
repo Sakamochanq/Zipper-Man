7a99334b4776eb8f801b89b1ee3ba7b43ebab206
Sakamochanq <sakamochan123@outlook.jp>
2026-06-12 21:08:11 +0900

    feat: プロジェクトファイルの追加

---
+++ src/Zipper-Man/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zipper_Man
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Source());
+        }
+    }
+}
